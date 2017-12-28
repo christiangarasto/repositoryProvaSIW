8f130618bae79c2066b38781031942f6d0e2b176
Christian Garasto <christiangarasto@gmail.com>
2017-12-28 18:40:23 +0100

    Il database funziona al 90%, le tabelle vengono create, la cancellazione funziona. Unico problema alcuni campi rimangono null nel findAll (deduco) -> da controllare

--- SIW_Progetto/test/persistence/MainJDBC.java
+++ SIW_Progetto/test/persistence/MainJDBC.java
@@ -1,6 +1,8 @@
 package persistence;
 
 
+import model.Evento;
+import model.Luogo;
 import model.Utente;
 import persistence.dao.EventoDao;
 import persistence.dao.LuogoDao;
@@ -16,22 +18,40 @@ public class MainJDBC {
 		util.createDatabase();
 		
 		UtenteDao utenteDao = factory.getUtenteDAO();
-//		EventoDao eventoDao = factory.getEventoDAO();
-//		LuogoDao luogoDao = factory.getLuogoDAO();
+			Utente utente1 = new Utente("00000001","Ciccio s.r.l");
+			Utente utente2 = new Utente("00000002", "Carletto s.p.a");
 
-		Utente utente1 = new Utente("00000001","Ciccio s.r.l");
+		LuogoDao luogoDao = factory.getLuogoDAO();
+			Luogo woodstock = new Luogo(utente1, "Woodstock", "cod1", "Cosenza", "Corigliano Calabro", "Via le mani dal naso 8");
 		
-		Utente utente2 = new Utente("00000002", "Carletto s.p.a");
-
+		
+		EventoDao eventoDao = factory.getEventoDAO();		
+			Evento rapinaDay = new Evento("Vi svaligiamo il portafogli", "ev1", new java.sql.Date(2001,10,02), woodstock);
+			Evento rapinaDay2 = new Evento("Vi svaligiamo il portafogli, per la seconda volta", "ev2", new java.sql.Date(2018,10,02), woodstock);
 
 		//CREATE
 		utenteDao.save(utente1);
 		utenteDao.save(utente2);
 		
+		luogoDao.save(woodstock);
+		
+		eventoDao.save(rapinaDay);
+		eventoDao.save(rapinaDay2);
+		
 		//RETRIEVE		
-		System.out.println("Elenco utenti");
+		System.out.println("---- Elenco utenti");
 		for(Utente u : utenteDao.findAll()) {
 			System.out.println(u.getpIva() + ", " + u.getNome());
+		}	
+		
+		System.out.println("\n\n---- Elenco Luoghi");
+		for(Luogo l : luogoDao.findAll()) {
+			System.out.println(l.toString());
+		}	
+		
+		System.out.println("\n\n---- Elenco Eventi");
+		for(Evento e : eventoDao.findAll()) {
+			System.out.println(e.toString());
 		}	
 	}
 }
