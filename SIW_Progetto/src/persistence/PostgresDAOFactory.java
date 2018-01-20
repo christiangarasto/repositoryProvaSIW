3b0d729a48d71922eb2ab503eeb5927c33691c05
Christian Garasto <christiangarasto@gmail.com>
2018-01-20 15:32:49 +0100

    Database remoto aggiunto

--- SIW_Progetto/src/persistence/PostgresDAOFactory.java
+++ SIW_Progetto/src/persistence/PostgresDAOFactory.java
@@ -18,7 +18,9 @@ class PostgresDAOFactory extends DAOFactory {
 			Class.forName("org.postgresql.Driver");//.newInstance();
 			//questi vanno messi in file di configurazione!!!	
 //			dataSource=new DataSource("jdbc:postgresql://52.39.164.176:5432/xx","xx","p@xx");
-			dataSource=new DataSource("jdbc:postgresql://localhost:5432/progettoSIW","postgres","postgres");
+			//dataSource=new DataSource("jdbc:postgresql://localhost:5432/progettoSIW","postgres","postgres");
+			
+			dataSource = new DataSource("jdbc:postgresql://packy.db.elephantsql.com:5432/kndusqco", "kndusqco", "iNG1Dly0_rVEmfLFlcRnLVgqePFCVQ7n");
 		} 
 		catch (Exception e) {
 			System.err.println("PostgresDAOFactory.class: failed to load MySQL JDBC driver\n"+e);
