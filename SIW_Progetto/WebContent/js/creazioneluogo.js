aa8cc508cd15d098071d7d6f302f682d0a706d06
Christian Garasto <christiangarasto@gmail.com>
2018-01-23 15:54:34 +0100

    Risolto il problema dell'api di google maps, aggiunta di un luogo completata.

--- SIW_Progetto/WebContent/js/creazioneluogo.js
+++ SIW_Progetto/WebContent/js/creazioneluogo.js
@@ -1,60 +1,87 @@
 $(window).on('load', function() {
-	
-					$(".aggiungiLuogoButton").on('click', function() {
-						$("#nomeLuogo").val('');
-						$("#provincia").val('');
-						$("#comune").val('');
-						$("#indirizzo").val('');
-					});
-
-					$("#salvaLuogo").on('click',function() {
+					$("#nuovoLuogo").on('click', function() {
 										var nomeLuogo = $("#nomeLuogo").val();
-										var provincia = $("#provincia").val();
-										var comune = $("#comune").val();
-										var indirizzo = $("#indirizzo").val();
 
-										alert(nomeLuogo + ", " + provincia + ", " + comune + ", " + indirizzo);
-										
+										var ind = $("#mapsearch").val();
+										var nVirgole = 0;
+
+										var indirizzo = "";
+										var comune = "";
+										var provincia = "";
+
+										var i = 0;
+
+										for (i; i < ind.length; i++) {
+
+											if (ind.charAt(i) == ',') {
+												nVirgole++;
+											}
+
+											if (nVirgole < 1) {
+												indirizzo += ind.charAt(i);
+											} else if (nVirgole < 2) {
+												comune += ind.charAt(i);
+
+											} else if (nVirgole < 3) {
+												provincia += ind.charAt(i);
+											}
+										}
+
+										comune = comune.substring(2,
+												comune.length);
+										provincia = provincia.substring(2,
+												provincia.length);
+
 										if (nomeLuogo.length == 0
 												|| provincia.length == 0
 												|| comune.length == 0
 												|| indirizzo.length == 0) {
 
 											alert("Compilare tutti i campi richiesti!");
+										} else if (provincia.length != 2) {
+
+											alert("Indirizzo inserito non valido!");
+
 										} else {
-											alert("saluti");
-											
-											$('#nuovoLuogo').modal('hide');
+
+											var luogoN = {
+												nomeLuogoInput : nomeLuogo,
+												provinciaInput : provincia,
+												comuneInput : comune,
+												indirizzoInput : indirizzo
+											};
+
 											$.ajax({
-												type : "POST",
-												url : "gestioneluoghi",
-												data : {
-													nomeLuogoInput : nomeLuogo,
-													provinciaInput : provincia,
-													comuneInput : comune,
-													indirizzoInput : indirizzo
-												},
-												success : function(risposta) {
-													risposta = Json.parse(risposta);
-													
-													var txt = "<tr>";
-														txt += "<td>" + risposta.nome + "</td>";
-														txt += "<td>" + risposta.provincia + "</td>";
-														txt += "<td>" + risposta.comune + "</td>";
-														txt += "<td>" + risposta.indirizzo + "</td>";
-														txt += "</tr>";
-													
-													
-													$("#elenco").html(txt);
-													
-													$("#nomeLuogo").val('');
-													$("#provincia").val('');
-													$("#comune").val('');
-													$("#indirizzo").val('');
-													
-													
-												}											
-											}); 
+														type : "POST",
+														url : "gestioneluoghi",
+														datatype : "json",
+														data : JSON.stringify(luogoN),
+														success : function(r) {
+
+															var luogo = JSON.parse(r);
+
+															var txt = "<tr>";
+															txt += "<td><div class=\"checkbox\"><label><input class=\"luogoDaEliminare\" type=\"checkbox\" value=\"";
+															txt += luogo.codice;
+															txt += "\"></label></div></td><td>";
+															txt += luogo.nome;
+															txt += "</td><td>";
+															txt += luogo.provincia;
+															txt += "</td><td>";
+															txt += luogo.comune;
+															txt += "</td><td>";
+															txt += luogo.indirizzo;
+															txt += "</td>";
+															txt += "</tr>";
+
+															$("#elenco")
+																	.append(txt);
+
+
+															$("#nomeLuogo").val('');
+															$("#mapsearch").val('');
+														}
+													});
 										}
 
 									});
